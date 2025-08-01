3f0b37764e7c3738c8bb8e14ece27f149e6d46ca
Vishwas Sonker <vishwasprofessional25@gmail.com>
2025-08-01 23:32:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.write("Enter student's CGPA and IQ to check if you might get placed.")
 # Take inputs
 
 cgpa = st.number_input("CGPA (0 - 10)", min_value=0.0, max_value=10.0, step=0.1)
-iq = st.number_input("IQ (0 - 100)", min_value=50, max_value=200, step=1)
+iq = st.number_input("IQ (50 - 200)", min_value=50, max_value=200, step=1)
 
 if st.button("Predict"):
     # Make prediction
